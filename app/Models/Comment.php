3fd8b45db28138f9ce597f1cdc442fa09376829f
Dude <mm.dawood166@gmail.com>
2024-03-01 16:25:08 +0200

    Delcare models relationships

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -2,10 +2,22 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Comment extends Model
 {
     use HasFactory;
+
+    protected $casts = [
+        'body' => 'array'
+    ];
+
+    public function post() {
+        return $this->belongsTo(Post::class, 'post_id');
+    }
+
+    public function user() {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,10 +2,30 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Post extends Model
 {
     use HasFactory;
+
+    protected $casts = [
+        'body' => 'array'
+    ];
+    
+    public function getTitleUpperCaseAttribute() {
+        return strtoupper($this->title);
+    }
+
+    public function setTitleAttribute($value) {
+        return $this->attributes['title'] = strtolower($value);
+    }
+    
+    public function comments() {
+        return $this->hasMany(Comment::class, 'post_id');
+    }
+
+    public function users() {
+        return $this->belongsToMany(User::class, 'post-user', 'post_id', 'user-id');
+    }
 }
