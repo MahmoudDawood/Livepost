c72e4e21ed8289476b7f48de5a804789a26f9c9b
Dude <mm.dawood166@gmail.com>
2024-03-05 14:26:31 +0200

    Create basic api routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,36 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::get('users', function () {
+    return new JsonResource([
+        "data" => "users"
+    ]);
+});
+
+Route::get('users/{user}', function ($user) {
+    return new JsonResource([
+        "data" => $user
+    ]);
+});
+
+
+Route::post('users', function (Request $request) {
+    return new JsonResource([
+        "data" => "posted"
+    ]);
+});
+
+Route::patch('users/{id}', function ($id) {
+    return new JsonResource([
+        "data" => "User ".$id." patched"
+    ]);
+});
+
+Route::delete('users/{id}', function ($id) {
+    return new JsonResource([
+        "data" => "User ".$id." deleted"
+    ]);
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
